3bf89ca6eea8841765eb5676e07d4bd9440bd939
Rudinei Costa <costaaugusto177@outlook.com>
2020-09-10 01:21:49 -0300

    feat: create appointments with valided date

--- src/routes/appoitments.routes.ts
+++ src/routes/appoitments.routes.ts
@@ -1,20 +1,37 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { v4 } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentRouter = Router();
 
-const appointments = [];
+interface IAppointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: IAppointment[] = [];
 
 appointmentRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+    const findAppointmenstInSameDate = appointments.find(appointment =>
+        isEqual(appointment.date, parsedDate),
+    );
+    if (findAppointmenstInSameDate) {
+        return response
+            .status(400)
+            .json({ message: 'This appointment is already booked ' });
+    }
+
     const appointment = {
-        id: uuid(),
+        id: v4(),
         provider,
-        date,
+        date: parsedDate,
     };
     appointments.push(appointment);
-    response.json(appointment);
+    return response.json(appointment);
 });
 
 export default appointmentRouter;
